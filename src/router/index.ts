700634a2fb366c65579c9777790881f3596dddac
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-02-09 01:58:24 -0800

    Convert router to static

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,41 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-// Dynamically import all view components
-const modules = import.meta.glob('../views/*.vue')
-
-const routes: RouteRecordRaw[] = Object.keys(modules).map((filepath) => {
-  const name = filepath.match(/\.\/views\/(.*)\.vue$/)?.[1]
-  const path = name === 'Home' ? '/' : `/${name?.toLowerCase()}`
-
-  return {
-    path,
-    name,
-    component: modules[filepath],
-  }
-})
+const routes: RouteRecordRaw[] = [
+  {
+    name: 'home',
+    path: '/',
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: () => import('@/views/About.vue'),
+    redirect: { name: 'about-summary' },
+    children: [
+      {
+        name: 'about-summary',
+        path: 'summary',
+        component: () => import('@/views/AboutSummary.vue'),
+      },
+      {
+        name: 'about-confidences',
+        path: 'confidences',
+        component: () => import('@/views/AboutConfidences.vue'),
+      },
+    ],
+  },
+  {
+    name: 'portfolio',
+    path: '/portfolio',
+    component: () => import('@/views/Portfolio.vue'),
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    component: () => import('@/views/Contact.vue'),
+  },
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
