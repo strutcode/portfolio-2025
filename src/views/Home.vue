a40295195b16e133a5de96a337c2f213f2e46ba0
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-03-01 19:59:00 -0800

    Experiment with a new tagline for the landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div>
     <canvas ref="canvas"></canvas>
     <div class="content">
-      <!-- <h1>Building Awesome Stuff</h1> -->
+      <div class="catchline">
+        To make a big <span class="big">impact</span> you need<br /><span class="big">talent</span>
+        that can keep pace with your vision
+      </div>
     </div>
   </div>
 </template>
@@ -37,7 +40,33 @@
   }
 
   .content {
-    position: relative;
-    z-index: +1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .catchline {
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    width: 70%;
+    transform: translate(-50%, -50%);
+    font-size: 5vh;
+    color: #eee;
+    text-align: left;
+    font-weight: 300;
+    text-shadow:
+      0.25rem 0 0.25rem rgba(0, 0, 0, 0.25),
+      -0.25rem 0 0.25rem rgba(0, 0, 0, 0.25),
+      0 0.25rem 0.25rem rgba(0, 0, 0, 0.25),
+      0 -0.25rem 0.25rem rgba(0, 0, 0, 0.25);
+
+    .big {
+      font-size: 6vh;
+      color: #fff;
+      font-weight: 400;
+    }
   }
 </style>
