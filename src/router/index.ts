567d5f68d73d91f296558c21f4ae041bc40f102b
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-03-03 19:11:07 -0800

    Fix build warnings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import App from '../App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
@@ -5,22 +6,22 @@ const routes: RouteRecordRaw[] = [
   {
     name: 'home',
     path: '/',
-    component: () => import('@/views/Home.vue'),
+    component: App,
   },
   {
     name: 'about',
     path: '/about',
-    component: () => import('@/views/About.vue'),
+    component: App,
   },
   {
     name: 'portfolio',
     path: '/portfolio',
-    component: () => import('@/views/Portfolio.vue'),
+    component: App,
   },
   {
     name: 'contact',
     path: '/contact',
-    component: () => import('@/views/Contact.vue'),
+    component: App,
   },
 ]
 
