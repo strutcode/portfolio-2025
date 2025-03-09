96afff11b21a72323d340063408a856f6a815851
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-03-09 15:16:03 -0700

    Clean up about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -37,11 +37,30 @@
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
             mollit anim id est laborum.
           </p>
+
+          <h1>Current Equipment</h1>
+          <div>Ryzen 9 5900HX</div>
+          <div>RTX 3080 16GB</div>
+          <div>64GB DDR4</div>
+          <div>2x 1TB Samsung NVMe SSD</div>
+
+          <h1>Proficiencies</h1>
+          <div>Testing and Test Driven Development</div>
+          <div>Debugging and Debug Tooling</div>
+          <div>Performance Analysis and Optimization</div>
+          <div>Asynchronous Programming/Multithreading</div>
+          <div>Project Lifecycle Management</div>
+          <div>Authentication & Cryptography</div>
+
+          <h1>Unique Abilities</h1>
+          <div>Embedded Systems</div>
+          <div>Operating System Programming</div>
+          <div>Programming Language Implementation</div>
         </div>
       </div>
     </div>
 
-    <div class="banner purple">Core Strengths</div>
+    <div class="banner purple">Equipped Skills</div>
     <div class="skills">
       <div class="skill shiny">
         <Icon icon="devicon:typescript" />
@@ -161,7 +180,7 @@
       </div>
     </div>
 
-    <div class="banner red">Notable Skills</div>
+    <div class="banner red">Skill Deck</div>
     <div class="notable">
       <div class="skill shiny">
         <Icon icon="devicon:elasticsearch" />
@@ -358,115 +377,6 @@
       <div class="skill shiny">
         <Icon icon="devicon:pnpm" />
         <div class="skillTitle">pnpm</div>
-      </div>
-    </div>
-
-    <div class="confidences">
-      <div>
-        <div class="title strong">Core Strengths</div>
-        <ul>
-          <li>Vue 2.x/3.x</li>
-          <li>JavaScript ES5/ES6/ESNext</li>
-          <li>TypeScript</li>
-          <li>Debugging/Debug Tooling</li>
-          <li>HTML 4/5 + XML</li>
-          <li>CSS 2.x/3.x</li>
-          <li>CSS Preprocessors (Sass/Less)</li>
-          <li>RESTful APIs</li>
-          <li>Asynchronous Code</li>
-          <li>Multithreading</li>
-          <li>Regular Expressions</li>
-          <li>Performance Analysis & Optimization</li>
-          <li>Git SCM</li>
-          <li>Docker + Containerization</li>
-          <li>Linux</li>
-          <li>Browser Compatibility</li>
-          <li>Game Development</li>
-          <li>Test-Driven Development (TDD)</li>
-          <li>Unit Testing</li>
-          <li>Integration Testing</li>
-          <li>End-to-End Testing</li>
-          <li>Code Quality Metrics</li>
-          <li>Test Coverage</li>
-          <li>Vite</li>
-          <li>Webpack</li>
-          <li>Mocha</li>
-          <li>Ava</li>
-          <li>Vitest</li>
-          <li>Github Actions</li>
-          <li>CircleCI</li>
-          <li>Godot/GDScript</li>
-        </ul>
-      </div>
-
-      <div>
-        <div class="title medium">Knowledgable/Past Experience</div>
-        <ul>
-          <li>React</li>
-          <li>Redux</li>
-          <li>Immutable</li>
-          <li>Jest</li>
-          <li>SQL</li>
-          <li>Postgres</li>
-          <li>MySQL</li>
-          <li>MariaDB</li>
-          <li>CouchDB</li>
-          <li>Mongo</li>
-          <li>SQLite</li>
-          <li>PHP</li>
-          <li>Laravel</li>
-          <li>GraphQL</li>
-          <li>Python</li>
-          <li>C/C++</li>
-          <li>Ruby</li>
-          <li>Flutter</li>
-          <li>Express</li>
-          <li>Ruby on Rails</li>
-          <li>Pug Templates</li>
-          <li>Handlebars Templates</li>
-          <li>C#</li>
-          <li>Cloud Computing (AWS, GCP,)</li>
-          <li>ElasticSearch</li>
-          <li>Project Management</li>
-          <li>Customer Support</li>
-          <li>Authentication + Cryptography</li>
-          <li>Memory Stores (Redis)</li>
-          <li>CI/CD (Circle/Jenkins)</li>
-          <li>Unity C#</li>
-          <li>Unreal Engine 4/5</li>
-        </ul>
-      </div>
-
-      <div>
-        <div class="title light">Other Skills</div>
-        <ul>
-          <li>Rust</li>
-          <li>Kubernetes</li>
-          <li>Capacitor/Ionic</li>
-          <li>x86 Assembly</li>
-          <li>Java</li>
-          <li>Network Architecture</li>
-          <li>AI Development</li>
-          <li>Graphic Design</li>
-          <li>Operating Systems Programming</li>
-          <li>Geographic Information Systems (GIS)</li>
-          <li>Embedded Systems</li>
-          <li>RxJS</li>
-          <li>Static Code Analysis</li>
-          <li>Abstract Syntax Trees</li>
-          <li>Programming Language Implementation</li>
-          <li>Wordpress</li>
-          <li>Drupal</li>
-          <li>Tailwind CSS</li>
-          <li>GitLab</li>
-          <li>WebRTC</li>
-          <li>Perl</li>
-          <li>Discord API</li>
-          <li>Slack API</li>
-          <li>Lua</li>
-          <li>Arduino</li>
-          <li>Flask</li>
-        </ul>
       </div>
     </div>
   </div>
