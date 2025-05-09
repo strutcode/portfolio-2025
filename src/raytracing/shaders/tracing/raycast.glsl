9212b6e2bfe24da9187565f08c48812a58802637
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-05-08 21:47:09 -0700

    Add some movement to make the scene more interesting

--- src/raytracing/shaders/tracing/raycast.glsl
+++ src/raytracing/shaders/tracing/raycast.glsl
@@ -8,12 +8,12 @@
 RayHit raycast(Ray ray) {
   // Define a sphere as the scene
   Sphere sphere;
-  sphere.position = vec3(-1.4, 0.0, 10.0);
+  sphere.position = vec3(0.0, 0.0, 10.0) + vec3(sin(time * 0.001) * 1.5, 0.0, cos(time * 0.001) * 1.5);
   sphere.color = vec3(1.0, 1.0, 1.0);
   sphere.radius = 1.2;
   sphere.roughness = 0.08;
   Sphere sphere2;
-  sphere2.position = vec3(1.4, 0.0, 10.0);
+  sphere2.position = vec3(0.0, 0.0, 10.0) + vec3(cos(time * 0.001) * 1.5, 0.0, sin(time * 0.001) * 1.5);
   sphere2.color = vec3(1.0, 1.0, 1.0);
   sphere2.radius = 1.2;
   sphere2.roughness = 0.08;
