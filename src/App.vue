996e2a3999120ebdb207c550a61e3499c2e0d751
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-05-10 14:13:21 -0700

    Improve theming with user preference and persistence

--- src/App.vue
+++ src/App.vue
@@ -23,10 +23,35 @@
   import Contact from './components/sections/Contact.vue'
   import Footer from './components/Footer.vue'
 
-  const isDarkTheme = ref(false)
+  // Check if the user has a preference for dark mode
+  function getDarkModePreference() {
+    const userTheme = localStorage.getItem('theme')
+    if (userTheme === 'dark') return true
+    if (userTheme === 'light') return false
+
+    const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    console.log('User prefers dark mode:', userPrefersDark)
+    return userPrefersDark
+  }
+
+  // Set the initial theme based on user preference
+  const isDarkTheme = ref(getDarkModePreference())
+
+  // Apply the theme to the document
+  const applyTheme = () => {
+    if (isDarkTheme.value) {
+      document.documentElement.classList.add('dark-theme')
+    } else {
+      document.documentElement.classList.remove('dark-theme')
+    }
+  }
 
   const toggleTheme = () => {
     isDarkTheme.value = !isDarkTheme.value
-    document.documentElement.classList.toggle('dark-theme')
+    applyTheme()
+    localStorage.setItem('theme', isDarkTheme.value ? 'dark' : 'light')
   }
+
+  // Set the initial state of the theme
+  applyTheme()
 </script>
