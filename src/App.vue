e0ef1f14dd9ccca965b71e9300169660bf7dea76
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-05-09 20:49:35 -0700

    Clean up early test elements

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,6 @@
   <Navigation :isDarkTheme="isDarkTheme" @toggleTheme="toggleTheme" />
   <Background />
   <HeroScene />
-  <div class="wrapper">
-    <GlassCard>
-      <h2>Sample title</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-    </GlassCard>
-  </div>
   <About />
   <Skills />
   <Contact />
@@ -37,10 +27,3 @@
     document.documentElement.classList.toggle('dark-theme')
   }
 </script>
-
-<style scoped>
-  .wrapper {
-    max-width: 1200px;
-    margin: 5rem auto;
-  }
-</style>
