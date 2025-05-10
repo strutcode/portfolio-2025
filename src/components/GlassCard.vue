5c30186a9a3ee69298dfd3bfcc387d1a1d981449
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-05-10 14:31:05 -0700

    Improve light mode contrast and readability

--- src/components/GlassCard.vue
+++ src/components/GlassCard.vue
@@ -6,12 +6,7 @@
 
 <style scoped>
   .glassCard {
-    background: linear-gradient(
-      to bottom right,
-      rgba(40, 40, 40, 0.22),
-      rgba(0, 0, 0, 0.12),
-      rgba(0, 0, 0, 0.22)
-    );
+    background: var(--glass-background);
     backdrop-filter: blur(8px);
     border: 2px outset white;
     border-radius: 10px;
