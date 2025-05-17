f6b760c8d0e5da4f795328c360e5dc5700d5b63f
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-05-17 10:48:22 -0700

    Convert scene renderer into a generic object system

--- src/hero/HeroScene.ts
+++ src/hero/HeroScene.ts
@@ -9,26 +9,20 @@ import {
   setBuffersAndAttributes,
   setUniforms,
   v3,
-  primitives,
   createBufferInfoFromArrays,
+  type ProgramInfo,
+  type BufferInfo,
 } from 'twgl.js'
 import WavefrontLoader from './WavefrontLoader'
 
 export default class HeroScene extends Scene {
+  private objects: { programInfo: ProgramInfo; bufferInfo: BufferInfo }[] = []
+
   protected async setup() {
     const gl = this.ctx
 
     // Create the shader
     const programInfo = createProgramInfo(gl, [vertex, fragment])
-
-    // Create an array buffer for the terrain
-    const bufferInfo = primitives.createCubeBufferInfo(gl, 2)
-
-    // Save data for use in the render step
-    this.renderData = {
-      programInfo,
-      bufferInfo,
-    }
 
     // Initialize the resize listener
     window.addEventListener('resize', this.boundResize)
@@ -36,7 +30,10 @@ export default class HeroScene extends Scene {
 
     // Load the model from the server
     WavefrontLoader.load('/terrain.obj').then((data) => {
-      this.renderData.bufferInfo = createBufferInfoFromArrays(gl, data)
+      this.objects.push({
+        programInfo,
+        bufferInfo: createBufferInfoFromArrays(gl, data),
+      })
     })
   }
 
@@ -58,7 +55,6 @@ export default class HeroScene extends Scene {
 
   protected render() {
     const gl = this.ctx
-    const { programInfo, bufferInfo } = this.renderData
     const darkMode = document.documentElement.classList.contains('dark-theme')
 
     const now = performance.now()
@@ -78,13 +74,7 @@ export default class HeroScene extends Scene {
     gl.enable(gl.DEPTH_TEST)
     gl.enable(gl.CULL_FACE)
 
-    // Activate the shader
-    gl.useProgram(programInfo.program)
-
-    // Prepare the mesh for rendering
-    setBuffersAndAttributes(gl, programInfo, bufferInfo)
-
-    // Calculate a perspective camera
+    // Calculate all global data
     const time = performance.now() / 1000
     const projection = m4.perspective(
       (30 * Math.PI) / 180,
@@ -95,21 +85,31 @@ export default class HeroScene extends Scene {
     const camera = m4.lookAt([1, 1.2, -8], [0, 1, 0], [0, 1, 0])
     const view = m4.inverse(camera)
     const viewProjection = m4.multiply(projection, view)
-    const world = m4.identity()
 
-    // Set up shader data
-    setUniforms(programInfo, {
-      time,
-      resolution: [this.canvas.width, this.canvas.height],
-      world,
-      worldViewProjection: m4.multiply(viewProjection, world),
-      lightDirection: v3.normalize(v3.create(0.5, -0.2, 1)),
-      shadowColor: darkMode ? [0.01, 0.03, 0.1] : [0.1, 0.3, 0.14],
-      lightColor: darkMode ? [0.05, 0.14, 0.05] : [0.6, 0.87, 0.6],
-    })
+    for (let object of this.objects) {
+      const { programInfo, bufferInfo } = object
+      const world = m4.identity()
 
-    // Draw
-    drawBufferInfo(gl, bufferInfo)
+      // Activate the shader
+      gl.useProgram(programInfo.program)
+
+      // Prepare the mesh for rendering
+      setBuffersAndAttributes(gl, programInfo, bufferInfo)
+
+      // Set up shader data
+      setUniforms(programInfo, {
+        time,
+        resolution: [this.canvas.width, this.canvas.height],
+        world,
+        worldViewProjection: m4.multiply(viewProjection, world),
+        lightDirection: v3.normalize(v3.create(0.5, -0.2, 1)),
+        shadowColor: darkMode ? [0.01, 0.03, 0.1] : [0.1, 0.3, 0.14],
+        lightColor: darkMode ? [0.05, 0.14, 0.05] : [0.6, 0.87, 0.6],
+      })
+
+      // Draw
+      drawBufferInfo(gl, bufferInfo)
+    }
 
     // Wait for the next render loop
     this.animationFrame = requestAnimationFrame(this.boundRender)
